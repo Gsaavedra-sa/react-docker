7186aa159038b5efab0892017d4f26e4fad2fac0
Gsaavedra-sa <gabrielsaito19@gmail.com>
2021-08-10 19:11:10 -0300

    implementando ssl nginx

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Desafio DevOps.
+        Desafio DevOps, link do repositorio abaixo.
         </p>
         <a
           className="App-link"
